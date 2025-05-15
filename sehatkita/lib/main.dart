4c5ae013b330320ac7947fbd1aff2fc3eb85fe72
YudiMubarok1 <yudimubarok1@student.unp.ac.id>
2025-05-15 10:32:55 +0700

    ini serius yg baru

--- sehatkita/lib/main.dart
+++ sehatkita/lib/main.dart
@@ -1,91 +1,122 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_messaging/firebase_messaging.dart'; // Import untuk FCM
-import 'screens/home_screen.dart';
-import 'screens/AddSchedule_screen.dart'; // Perbaiki import halaman tambah jadwal
-//import 'screens/history_screen.dart'; // Import halaman riwayat pemeriksaan
-import 'screens/profile_screen.dart'; // Import halaman profil pengguna
-import 'screens/EditSchedule_screen.dart';
-import 'package:sehatkita/screens/users_screen.dart'; // Import halaman daftar pengguna
-import 'package:sehatkita/screens/OtpVerification_screen.dart'; // Import halaman OTP verification
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(); // Menginisialisasi Firebase
-  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
-  runApp(MyApp());
+void main() {
+  runApp(const MyApp());
 }
 
-// Fungsi untuk menangani notifikasi saat aplikasi berjalan di background
-Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
-  print("Handling a background message: ${message.messageId}");
-}
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
-class MyApp extends StatefulWidget {
+  // This widget is the root of your application.
   @override
-  _MyAppState createState() => _MyAppState();
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // TRY THIS: Try running your application with "flutter run". You'll see
+        // the application has a purple toolbar. Then, without quitting the app,
+        // try changing the seedColor in the colorScheme below to Colors.green
+        // and then invoke "hot reload" (save your changes or press the "hot
+        // reload" button in a Flutter-supported IDE, or press "r" if you used
+        // the command line to start the app).
+        //
+        // Notice that the counter didn't reset back to zero; the application
+        // state is not lost during the reload. To reset the state, use hot
+        // restart instead.
+        //
+        // This works for code too, not just values: Most code changes can be
+        // tested with just a hot reload.
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
 }
 
-class _MyAppState extends State<MyApp> {
-  // Firebase Messaging instance
-  final FirebaseMessaging messaging = FirebaseMessaging.instance;
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
 
   @override
-  void initState() {
-    super.initState();
-    _getFcmToken();
-  }
+  State<MyHomePage> createState() => _MyHomePageState();
+}
 
-  // Mendapatkan token FCM setelah Firebase diinisialisasi
-  Future<void> _getFcmToken() async {
-    String? token = await messaging.getToken();
-    print("FCM Token: $token");
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
 
-    // Simpan token FCM di Firestore atau SharedPreferences jika perlu
-    if (token != null) {
-      await _saveFcmToken(token);
-    }
-  }
-
-  // Fungsi untuk menyimpan token FCM ke Firestore
-  Future<void> _saveFcmToken(String token) async {
-    // Menyimpan token ke Firestore setelah pengguna berhasil login atau terautentikasi
-    FirebaseFirestore.instance.collection('users').doc('user_uid').update({
-      'fcmToken': token, // Menyimpan token FCM pada koleksi pengguna
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'SehatKita',
-      theme: ThemeData(primarySwatch: Colors.green),
-      initialRoute: '/', // Tentukan rute awal
-      routes: {
-        '/': (context) => HomeScreen(), // Halaman awal (HomeScreen)
-        '/home':
-            (context) =>
-                HomeScreen(), // Rute ke halaman utama setelah verifikasi
-        '/users':
-            (context) => UsersScreen(), // Rute untuk halaman daftar pengguna
-        '/addSchedule':
-            (context) =>
-                AddScheduleScreen(), // Rute untuk halaman tambah jadwal
-        '/history':
-            (context) =>
-                HistoryScreen(), // Rute untuk halaman riwayat pemeriksaan
-        '/profile':
-            (context) => ProfileScreen(), // Rute untuk halaman profil pengguna
-        '/editSchedule':
-            (context) => EditScheduleCheckupScreen(
-              id: 'schedule_id', // Ganti dengan ID yang sesuai
-              type: 'schedule',
-            ), // Rute untuk halaman penjadwalan ulang pemeriksaan
-        '/otpVerification':
-            (context) =>
-                OtpVerificationScreen(), // Rute untuk halaman OTP verification
-      },
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        // TRY THIS: Try changing the color here to a specific color (to
+        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
+        // change color while the other colors stay the same.
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+          // Column is also a layout widget. It takes a list of children and
+          // arranges them vertically. By default, it sizes itself to fit its
+          // children horizontally, and tries to be as tall as its parent.
+          //
+          // Column has various properties to control how it sizes itself and
+          // how it positions its children. Here we use mainAxisAlignment to
+          // center the children vertically; the main axis here is the vertical
+          // axis because Columns are vertical (the cross axis would be
+          // horizontal).
+          //
+          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
+          // action in the IDE, or press "p" in the console), to see the
+          // wireframe for each widget.
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text('You have pushed the button this many times:'),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
